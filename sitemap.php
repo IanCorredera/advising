b0fcdf72c386f549dde94df8633f4a945ede28b6
IanCorredera <ian.corredera@gmail.com>
2015-10-20 20:40:28 -0400

    tables.php

---
+++ sitemap.php
@@ -0,0 +1,43 @@
+
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/css/base.css">base.css</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/css/reset.css">reset.css</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/lib/constants.php">constants.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/lib/custom-functions.php">custom-functions.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/lib/security.php">security.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/sql/enrollments.sql">enrollments.sql</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/sql/studentTable.sql">studentTable.sql</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/sql/tblCourses.sql">tblCourses.sql</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/sql/tblSections.sql">tblSections.sql</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/tblTeachers.sql">tblTeachers.sql</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/footer.php">footer.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/header.php">header.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/index.php">index.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/nav.php">nav.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/populate-table.php">populate-table.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/tables.php">tables.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/top.php">top.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/bin/Database.php">Database.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/pass.php">pass.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/lib/mailMessage.php">mailMessage.php</a>
+
+<a href="https://icorrede.w3.uvm.edu/cs148/assignment1.0/tryme.php">tryme.php</a>
